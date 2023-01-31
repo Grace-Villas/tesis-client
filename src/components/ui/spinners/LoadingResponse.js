a8b949acb70bc3147bed3a6036499295164edeae
mjp0997 <mjp0997@gmail.com>
2023-01-31 13:54:31 -0400

    Spinner para esperar respuesta

---
+++ src/components/ui/spinners/LoadingResponse.js
@@ -0,0 +1,23 @@
+
+
+
+const LoadingResponse = ({state}) => {
+
+   if (!state) {
+      return (
+         <></>
+      );
+   }
+
+   return (
+      <div className='response-container'>
+         <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+         </div>
+      </div>
+   );
+}
+
+
+
+export default LoadingResponse;
