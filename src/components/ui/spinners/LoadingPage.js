e6531b55d1817a8610716a0e0579abd2b5faff8a
mjp0997 <mjp0997@gmail.com>
2023-02-02 17:25:07 -0400

    Estilos del spinner pre vista

---
+++ src/components/ui/spinners/LoadingPage.js
@@ -0,0 +1,17 @@
+
+
+
+const LoadingPage = () => {
+
+   return (
+      <div className='loading-page-container'>
+         <div className="spinner-border text-primary" role="status">
+            <span className="visually-hidden">Loading...</span>
+         </div>
+      </div>
+   );
+}
+
+
+
+export default LoadingPage;
