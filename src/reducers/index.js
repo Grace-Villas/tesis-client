2c2b9de00fcf614f35597675795ba7209956859b
mjp0997 <mjp0997@gmail.com>
2023-02-02 17:13:27 -0400

    Anidando reducer de configuraciones

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,9 +1,11 @@
 import { authReducer } from './authReducer';
+import { settingsReducer } from './settingsReducer';
 import { uiReducer } from './uiReducer';
 
 
 
 export {
    authReducer,
+   settingsReducer,
    uiReducer,
 }
