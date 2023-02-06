60faa8af759657c9c013094ff0a87dcdd099785d
mjp0997 <mjp0997@gmail.com>
2023-02-06 13:35:35 -0400

    Integrada funcionalidad con redux

--- src/components/hud/BreadCrumb.js
+++ src/components/hud/BreadCrumb.js
@@ -1,23 +1,45 @@
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 
 
 const BreadCrumbs = () => {
+   
+   const { breadcrumb } = useSelector(state => state.ui);
 
-   // TODO: funcionalidad con redux
+   const [current, setCurrent] = useState('');
+
+   useEffect(() => {
+      setCurrent(breadcrumb.length > 0 ? breadcrumb[breadcrumb.length - 1].text : '');
+   }, [breadcrumb]);
 
    return (
-      <div className="content-header d-flex flex-row flex-content-between">
-         <div className="col-12">
-            <h2 className="content-header-title float-start mb-0">Home</h2>
+      <div className='content-header d-flex flex-row flex-content-between'>
+         <div className='col-12'>
+            <h2 className='content-header-title float-start mb-0'>{current.toLocaleUpperCase()}</h2>
 
-            <div className="breadcrumb-wrapper">
-               <ol className="breadcrumb">
-                  <li className="breadcrumb-item">
-                     <a href="index.html">Home</a>
-                  </li>
+            <div className='breadcrumb-wrapper'>
+               <ol className='breadcrumb'>
+                  {
+                     breadcrumb.map((bread, i) => {
+                        if (i === breadcrumb.length - 1) {
+                           return
+                        }
 
-                  <li className="breadcrumb-item active">
-                     Index
+                        return (
+                           <li
+                              key={'breadbrum-' + bread.link}
+                              className='breadcrumb-item'
+                           >
+                              <Link to={bread.link}>{bread.text}</Link>
+                           </li>
+                        )
+                     })
+                  }
+
+                  <li className='breadcrumb-item active'>
+                     {current}
                   </li>
                </ol>
             </div>
