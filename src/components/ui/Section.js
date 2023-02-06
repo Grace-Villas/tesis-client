beadc8bda9e0bad5156f9445521dd1ab42fe270f
mjp0997 <mjp0997@gmail.com>
2023-02-06 19:38:27 -0400

    Agregando proptypes y eliminando margin

--- src/components/ui/Section.js
+++ src/components/ui/Section.js
@@ -4,12 +4,12 @@ import PropTypes from 'prop-types';
 /**
  * Componente sección. Fondo blanco bordeado y padding
  * @param {} props 
- * @returns 
+ * @returns {Element}
  */
-const Section = ({children, size, sizeDesk, sizeTablet, classes}) => {
+const Section = ({children, size, sizeDesk, sizeTablet, className}) => {
 
    return (
-      <section className={`bg-white bg-gradient rounded-3 shadow-sm my-2 p-2 overflow-hidden ${size} ${sizeTablet} ${sizeDesk} ${classes}`}>
+      <section className={`bg-white bg-gradient rounded-3 shadow-sm p-2 overflow-hidden ${size} ${sizeTablet} ${sizeDesk} ${className}`}>
          {children}
       </section>
    );
@@ -18,15 +18,23 @@ const Section = ({children, size, sizeDesk, sizeTablet, classes}) => {
 
 
 Section.propTypes = {
+   children: PropTypes.node,
+
    size: PropTypes.string,
    sizeDesk: PropTypes.string,
-   sizeTablet: PropTypes.string
+   sizeTablet: PropTypes.string,
+
+   className: PropTypes.string
 };
 
 Section.defaultProps = {
-   size: "",
-   sizeDesk: "",
-   sizeTablet: "",
+   children: <></>,
+
+   size: '',
+   sizeDesk: '',
+   sizeTablet: '',
+
+   className: ''
 };
 
 
