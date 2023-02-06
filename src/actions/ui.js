3b07ffb3899353b911624da0933f08a0f6285ef3
mjp0997 <mjp0997@gmail.com>
2023-02-06 13:35:21 -0400

    Funcionalidad del breadcrumb

--- src/actions/ui.js
+++ src/actions/ui.js
@@ -8,3 +8,8 @@ export const toggleSidebar = (state) => ({
    type: types.SIDE_BAR,
    payload: state
 });
+
+export const setBreadcrumb = (breadcrumb) => ({
+   type: types.SET_BREADCRUMB,
+   payload: breadcrumb
+});
--- src/reducers/uiReducer.js
+++ src/reducers/uiReducer.js
@@ -1,13 +1,19 @@
 export const types = {
    // Mobile menu
    SIDE_BAR: '[UI] set sidebar state',
+
+   // Breadcrumb
+   SET_BREADCRUMB: '[UI] set breadcrumb data'
 }
 
 
 
 const initialState = {
    // sidebar
-   sidebar: false
+   sidebar: false,
+
+   // breadcrumb
+   breadcrumb: []
 }
 
 
@@ -18,6 +24,12 @@ export const uiReducer = (state = initialState, action) => {
          return {
             ...state,
             sidebar: action.payload
+         }
+
+      case types.SET_BREADCRUMB:
+         return {
+            ...state,
+            breadcrumb: action.payload
          }
 
       default:
