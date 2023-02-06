41d8b61e8f1c6228ae2e12e4020505962c7dbc7c
mjp0997 <mjp0997@gmail.com>
2023-02-05 21:04:08 -0400

    Componente Input

--- src/components/form/Input.js
+++ src/components/form/Input.js
@@ -1,28 +1,71 @@
+import PropTypes from 'prop-types';
 
 
 
-const Input = ({value, setValue, title, type, placeholder}) => {
-
-   // is-invalid (input)
-   // is-valid (input)
+const Input = ({
+   value, setValue, title, type, placeholder,
+   containerClass,
+   error, isValid
+}) => {
 
    const handleValue = (e) => setValue(e.target.value);
 
+   const handleInputClass = () => {
+      let className = 'form-control';
+
+      if (error) {
+         className += ' is-invalid';
+      } else if (isValid) {
+         return className += ' is-valid';
+      }
+
+      return className;
+   }
+
    return (
-      <div>
-         <label className="form-label" htmlFor="basicInput">{title}</label>
+      <div className={containerClass}>
+         <label className="form-label" htmlFor={title}>{title}</label>
          
          <input
             type={type}
-            className="form-control"
+            className={handleInputClass()}
             placeholder={placeholder}
             value={value}
             onChange={handleValue}
+            id={title}
          />
 
-         <div className='invalid-feedback'>valido</div>
+         {
+            error && (
+               <div className='invalid-feedback'>{error}</div>
+            )
+         }
       </div>
    );
+}
+
+
+
+Input.propTypes = {
+   value: PropTypes.string.isRequired,
+   setValue: PropTypes.func.isRequired,
+   title: PropTypes.string.isRequired,
+   type: PropTypes.oneOf(['text', 'number']),
+   placeholder: PropTypes.string,
+
+   containerClass: PropTypes.string,
+
+   error: PropTypes.string,
+   isValid: PropTypes.bool
+}
+
+Input.defaultProps = {
+   type: 'text',
+
+   containerClass: '',
+
+   error: null,
+   isValid: null
 }
 
 
