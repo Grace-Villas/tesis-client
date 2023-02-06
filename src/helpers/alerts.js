df77956fc90eee1999cb71d64389b18fe3c10af9
mjp0997 <mjp0997@gmail.com>
2023-02-06 16:07:44 -0400

    Toast de error y modal de confirmación

--- src/helpers/alerts.js
+++ src/helpers/alerts.js
@@ -2,13 +2,17 @@ import Swal from 'sweetalert2';
 
 
 
-const swalWithBootstrapButtons = Swal.mixin({
+const simpleToastSwal = Swal.mixin({
    customClass: {
       title: 'fs-6 m-0 fw-normal',
+      actions: 'gap-1',
       confirmButton: 'btn btn-primary p-50 px-2',
-      cancelButton: 'btn btn-danger'
+      cancelButton: 'btn btn-danger p-50 px-2',
+      htmlContainer: 'm-0'
    },
-   buttonsStyling: false
+   buttonsStyling: false,
+   confirmButtonText: 'Confirmar',
+   cancelButtonText: 'Cancelar',
 });
 
 /**
@@ -16,7 +20,7 @@ const swalWithBootstrapButtons = Swal.mixin({
  * @param {string} message Mensaje a mostrar en el toast
  */
 export const simpleSuccessToast = (message) => {
-   swalWithBootstrapButtons.fire({
+   simpleToastSwal.fire({
       icon: 'success',
       title: message,
       toast: true,
@@ -24,5 +28,69 @@ export const simpleSuccessToast = (message) => {
       position: 'bottom-end',
       width: 300,
       showConfirmButton: false,
+   });
+}
+
+
+
+const simpleDialogSwal = Swal.mixin({
+   customClass: {
+      title: 'fs-4 m-0',
+      actions: 'gap-1',
+      confirmButton: 'btn btn-primary p-50 px-2',
+      cancelButton: 'btn btn-danger p-50 px-2',
+      htmlContainer: 'm-0 fs-6'
+   },
+   buttonsStyling: false,
+   confirmButtonText: 'Confirmar',
+   cancelButtonText: 'Cancelar',
+});
+
+/**
+ * Modal sencillo de confirmación de acción
+ * @param {string} icon Ícono del modal
+ * @param {string} title Título del modal
+ * @param {string} text Texto a mostrar en el modal
+ * @returns {Promise<{isConfirmed: boolean, isDenied: boolean, isDismissed: boolean}>}
+ */
+export const simpleConfirmDialog = async (icon, title, text) => {
+   return await simpleDialogSwal.fire({
+      title: title,
+      text: text,
+      icon: icon,
+      showCancelButton: true,
+      width: 300
+   });
+}
+
+
+
+const arraySwal = Swal.mixin({
+   customClass: {
+      title: 'fs-6 m-0 mb-25',
+      actions: 'gap-1',
+      confirmButton: 'btn btn-primary p-50 px-2',
+      cancelButton: 'btn btn-danger p-50 px-2',
+      htmlContainer: 'm-0 d-flex flex-column gap-25'
+   },
+   buttonsStyling: false,
+   confirmButtonText: 'Confirmar',
+   cancelButtonText: 'Cancelar',
+});
+
+/**
+ * Toast posicionado en la esquina inferior derecha para mostrar un listado de errores (Ícono y mensajes)
+ * @param {Array<string>} errors Arreglo de errores a mostrar
+ */
+export const arrayErrorToast = (errors) => {
+   arraySwal.fire({
+      icon: 'error',
+      title: 'Ha ocurrido un error',
+      html: `${errors.map((error) => `<span>- ${error}</span>`).join('')}`,
+      toast: true,
+      timer: 3000,
+      position: 'bottom-end',
+      width: 300,
+      showConfirmButton: false
    });
 }
