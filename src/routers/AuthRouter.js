21c0d754541ad2893cbde8ecb3e6a527cca51685
mjp0997 <mjp0997@gmail.com>
2023-02-23 23:37:04 -0400

    Verficar instalación
    agregando wrapper para verificar la instalación y vista de instalación

--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -2,7 +2,13 @@ import { Navigate, Route, Routes } from 'react-router-dom';
 
 
 
+// Components
+import CheckInstallation from '../components/functionals/CheckInstallation';
+
+
+
 // Views
+import Installation from '../views/auth/Installation';
 import Login from '../views/auth/Login';
 import LostPassword from '../views/auth/LostPassword';
 import PasswordRecovery from '../views/auth/PasswordRecovery';
@@ -19,15 +25,19 @@ const AuthRouter = () => {
    useAuthenticated(false);
 
    return (
-      <Routes>
-         <Route path='/login' element={<Login />} />
+      <CheckInstallation>
+         <Routes>
+            <Route path='/login' element={<Login />} />
 
-         <Route path='/lost-password' element={<LostPassword />} />
+            <Route path='/lost-password' element={<LostPassword />} />
 
-         <Route path='/password-recovery' element={<PasswordRecovery />} />
-      
-         <Route path='*' element={<Navigate to='/' replace />} />
-      </Routes>
+            <Route path='/password-recovery' element={<PasswordRecovery />} />
+
+            <Route path='/installation' element={<Installation />} />
+            
+            <Route path='*' element={<Navigate to='/' replace />} />
+         </Routes>
+      </CheckInstallation>
    );
 }
 
