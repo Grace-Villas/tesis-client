f4ff396473971e4ca2bbf3fcd1713de2fe3bef74
mjp0997 <mjp0997@gmail.com>
2023-02-23 23:37:17 -0400

    Modal de instalación
    Modal de instalación exitosa

--- src/helpers/alerts.js
+++ src/helpers/alerts.js
@@ -94,3 +94,34 @@ export const arrayErrorToast = (errors) => {
       showConfirmButton: false
    });
 }
+
+
+
+const installationDialog = Swal.mixin({
+   customClass: {
+      title: 'fs-4 m-0',
+      actions: 'gap-1',
+      confirmButton: 'btn btn-primary p-50 px-2',
+      cancelButton: 'btn btn-danger p-50 px-2',
+      htmlContainer: 'm-0 fs-6 p-2'
+   },
+   buttonsStyling: false,
+   confirmButtonText: 'Confirmar',
+   cancelButtonText: 'Cancelar',
+});
+
+/**
+ * Modal sencillo de confirmación de acción
+ * @param {string} icon Ícono del modal
+ * @param {string} title Título del modal
+ * @param {string} text Texto a mostrar en el modal
+ * @returns {Promise<{isConfirmed: boolean, isDenied: boolean, isDismissed: boolean}>}
+ */
+export const installationConfirmDialog = async (icon, title, text) => {
+   return await installationDialog.fire({
+      title: title,
+      text: text,
+      icon: icon,
+      width: 400
+   });
+}
