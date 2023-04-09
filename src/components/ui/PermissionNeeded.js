98e3967401d3e1fba58a8f2153e350721127d9ac
Mario José Pineda <72577859+mjp0997@users.noreply.github.com>
2023-04-08 22:50:12 -0400

    Componente funcional para verificación de permisos
    Dependiendo de los parámetros suministrados permite esconder o mostrar un componente según el usuario tenga los permisos necesarios y sea o no un administrador

---
+++ src/components/ui/PermissionNeeded.js
@@ -0,0 +1,62 @@
+import { useSelector } from 'react-redux';
+import PropTypes from 'prop-types';
+
+
+
+/**
+ * Componente para controlar el renderizado de los componentes según
+ * el tipo de usuario y los permisos del usuario autenticado
+ * @param {{
+ *    children: Element,
+ *    onlyAdmin: boolean,
+ *    onlyClient: boolean,
+ *    section: string,
+ *    permission: string
+ * }} params Parámetros del componente 
+ * @returns 
+ */
+const PermissionNeeded = ({children, onlyAdmin, onlyClient, section, permission}) => {
+   
+   const { isAdmin, permissions } = useSelector(state => state.auth);
+
+   if (typeof onlyAdmin !== 'undefined' && (onlyAdmin && !isAdmin)) {
+      return (<></>);
+   }
+
+   if (typeof onlyClient !== 'undefined' && (onlyClient && isAdmin)) {
+      return (<></>);
+   }
+
+   if (typeof section !== 'undefined' && !permissions[section]) {
+      return (<></>);
+   }
+
+   if (typeof permission !== 'undefined' && !permissions[section][permission]) {
+      return (<></>);
+   }
+
+   return (
+      <>{children}</>
+   );
+}
+
+
+
+PermissionNeeded.propTypes = {
+   children: PropTypes.node.isRequired,
+   onlyAdmin: PropTypes.bool,
+   onlyClient: PropTypes.bool,
+   section: PropTypes.string,
+   permission: PropTypes.string,
+}
+
+PermissionNeeded.defaultProps = {
+   onlyAdmin: undefined,
+   onlyClient: undefined,
+   section: undefined,
+   permission: undefined,
+}
+
+
+
+export default PermissionNeeded;
